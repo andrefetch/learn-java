07166fd939c340acd2ee95cd9537b715bfd8d501
andrefetch <andresilvapersonal2007@gmail.com>
2026-09-25 17:18:58 -0400

    Added: Intro to Java

---
+++ src/Introduction/Main.java
@@ -0,0 +1,28 @@
+package Introduction;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Comments are used by //, this is my first Java program!
+
+		/*
+		Multi Line comment, so cool.
+		!
+		@
+		?
+		 */
+
+		// sout is a quick way to auto gen system.out.println()
+
+		System.out.println("I Love Pizza!");
+		System.out.println("It's really good");
+		System.out.println("Buy me pizza.");
+		System.out.println();
+
+		// homework section
+		System.out.println("COmment one");
+		System.out.println("COmment two");
+		System.out.println("COmment three");
+	}
+}
