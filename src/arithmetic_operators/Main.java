c4202b0b8d075cfa6aa89301bf02ca6686285628
andrefetch <andresilvapersonal2007@gmail.com>
2026-09-25 19:04:36 -0400

    Added: Arithmatic Operators

---
+++ src/arithmetic_operators/Main.java
@@ -0,0 +1,18 @@
+package arithmetic_operators;
+
+public class Main {
+    public static void main(String[] args){
+
+        int x = 10;
+        int y = 3;
+
+
+        //x += y;
+        //x -= y;
+        //x *= y;
+        x /= y;
+        x++;
+
+        System.out.println(x);
+    }
+}
